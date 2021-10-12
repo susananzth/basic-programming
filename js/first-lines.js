d57c580ad242fd3d40cc317234dbfebfb270faad
Susana Piñero <susananzth@gmail.com>
2021-10-11 21:36:21 -0500

    Primeras línas de código con JS

---
+++ js/first-lines.js
@@ -0,0 +1,19 @@
+// Creando un mensaje de alerta en el navegador
+alert('Mi nombre es: Susana :3');
+
+// Guardando el nombre en una variable para ser utilizada más adelante.
+var nombre = 'Susana';
+
+// Mensaje de navegador concatenendo el texto con la variable 'nombre'
+// Se utiliza el símbolo + para concatenar.
+alert('Mi nombre es: '+nombre+' con una variable :3');
+
+// Las variables pueden ser modificadas en el transcurso de la ejecución del programa.
+nombre = 'Josefina';
+alert('Reescribiendo el nombre: '+nombre+' almacenado en una variable :3');
+
+// Realizando operaciones:
+let x = 1;
+let y = 3;
+let resultado = x + y;
+alert('El resultado de "'+x+'" + "'+y+'" es: '+resultado);
